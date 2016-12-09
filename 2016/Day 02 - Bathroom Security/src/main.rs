adf50679811a82d11a501fac8678059891aed1e5
Sylvain Laperche <sylvain.laperche@gmail.com>
2016-12-09 18:59:45 +0100

    2016/02: reorganize code to prepare for part 2
    Since we have to deal with another kind of keypad in the part 2, we now
    have a structure `Keypad` which is responsible for providing a starting
    position, checking if a given position makes sense and, eventually,
    returning the key at a given position.
    
    Also, since the next keypad is alphanumeric and doesn't have a key in
    each position, we change the type of the key from i32 to Option<char>.

--- 2016/Day 02 - Bathroom Security/src/main.rs
+++ 2016/Day 02 - Bathroom Security/src/main.rs
@@ -12,57 +12,111 @@
 // }}}
 // {{{ Crates
 
-use std::cmp;
+#[macro_use]
+extern crate lazy_static;
+
 use std::fs::File;
 use std::io::Read;
 
 // }}}
-// {{{ Finger
+// {{{ Keypad
+
+/// Represents a location on the keypad.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+struct Point {
+    x:   i8,
+    y:   i8,
+}
+
+/// A square keypad that controls the access to a room.
+struct Keypad {
+    keys: Vec<Option<char>>, // Keys layout.
+    size: i8,                // Size of the keypad's side.
+    key_5 : Point,           // Position of the key "5".
+}
+
+impl Keypad {
+    /// Creates a keypad with the specified size and layout.
+    fn new(keys: &[Option<char>], side: i8, starting_pos: Point) -> Keypad {
+        assert_eq!((side*side) as usize, keys.len());
+
+        let mut layout = Vec::with_capacity(keys.len());
+        layout.extend(keys.iter().cloned());
+
+        Keypad { keys: layout, size: side, key_5: starting_pos }
+    }
+
+    /// Return the starting position (i.e the position of the key "5").
+    fn starting_pos(&self) -> Point {
+        self.key_5
+    }
+
+    /// Checks if the position is valid.
+    ///
+    /// A position is valid if it is on the keypad, over a key.
+    fn position_is_valid(&self, position: Point) -> bool {
+        let idx = position.y * self.size + position.x;
+
+        position.y >= 0 && position.y < self.size &&
+        position.x >= 0 && position.x < self.size &&
+        self.keys[idx as usize].is_some()
+    }
+
+    fn get_key_at(&self, position: Point) -> char {
+        let idx = position.y * self.size + position.x;
+        self.keys[idx as usize].unwrap()
+    }
+}
 
 /// The keypad to unlock the bathroom.
-const KEYPAD: [[i32; 3]; 3] = [[1, 2, 3],
-                               [4, 5, 6],
-                               [7, 8, 9]];
+lazy_static! {
+    /// Represent the following keypad:
+    /// ┌───────┐
+    /// │1  2  3│
+    /// │4  5  6│
+    /// │7  8  9│
+    /// └───────┘
+    static ref SIMPLE_KEYPAD: Keypad = {
+        Keypad::new(&[Some('1'), Some('2'), Some('3'),
+                      Some('4'), Some('5'), Some('6'),
+                      Some('7'), Some('8'), Some('9')],
+                    3, Point { x: 1, y: 1 })
+    };
+}
+
+// }}}
+// {{{ Finger
 
 /// Represents the finger's location on the keypad.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 struct Finger {
-    x: i8,
-    y: i8,
+    position: Point,
 }
 
 impl Finger {
-    /// Creates a finger at the initial position (over the 5).
-    fn new() -> Finger {
-        Finger { x: 0, y: 0 }
+    /// Creates a finger.
+    fn new(starting_pos: Point) -> Finger {
+        Finger { position: starting_pos }
     }
 
     /// Move the finger over the keypad.
-    ///
-    /// The keypad is represented as follow:
-    ///
-    ///   -1  0 +1
-    ///   ┌───────┐
-    /// -1│1  2  3│
-    ///  0│4  5  6│
-    /// +1│7  8  9│
-    ///   └───────┘
-    fn mv(&mut self, movement: Move) {
+    fn mv(&mut self, movement: Move, keypad: &Keypad) {
+        let mut target = self.position;
+
         match movement {
-            Move::Up    => self.y = cmp::max(self.y - 1, -1),
-            Move::Down  => self.y = cmp::min(self.y + 1,  1),
-            Move::Left  => self.x = cmp::max(self.x - 1, -1),
-            Move::Right => self.x = cmp::min(self.x + 1,  1),
+            Move::Up    => target.y -= 1,
+            Move::Down  => target.y += 1,
+            Move::Left  => target.x -= 1,
+            Move::Right => target.x += 1,
+        }
+        if keypad.position_is_valid(target) {
+            self.position = target;
         }
     }
 
     /// Returns the value of the key under the finger.
-    fn press_key(&self) -> i32 {
-        // +1 because indexing starts at 0, not -1.
-        let i = self.y + 1;
-        let j = self.x + 1;
-
-        KEYPAD[i as usize][j as usize]
+    fn press_key(&self, keypad: &Keypad) -> char {
+        keypad.get_key_at(self.position)
     }
 }
 
@@ -97,16 +151,16 @@ impl From<char> for Move {
 
 // }}}
 
-fn follow_instructions(instructions: &str) -> i32 {
-    let mut code = 0;
-    let mut pinky = Finger::new();
+fn follow_instructions(instructions: &str, keypad: &Keypad) -> String {
+    let mut code  = String::new();
+    let mut pinky = Finger::new(keypad.starting_pos());
 
     for line in instructions.lines() {
         let moves: Vec<Move> = line.chars().map(|c| Move::from(c)).collect();
         for movement in moves {
-            pinky.mv(movement);
+            pinky.mv(movement, keypad);
         }
-        code = code*10 + pinky.press_key();
+        code.push(pinky.press_key(keypad));
     }
     code
 }
@@ -116,14 +170,15 @@ fn main() {
     let mut input = String::new();
 
     file.read_to_string(&mut input).unwrap();
-    println!("{}", follow_instructions(&input));
+    println!("{}", follow_instructions(&input, &SIMPLE_KEYPAD));
 }
 
 // {{{ Tests
 
 #[test]
 fn examples_part1() {
-    assert_eq!(follow_instructions("ULL\nRRDDD\nLURDL\nUUUUD"), 1985);
+    assert_eq!(follow_instructions("ULL\nRRDDD\nLURDL\nUUUUD", &SIMPLE_KEYPAD),
+               "1985");
 }
 
 // }}}
