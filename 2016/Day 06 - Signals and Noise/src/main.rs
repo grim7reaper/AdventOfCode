4d864e39f767ad6123d9060696f2bb950f41e071
Sylvain Laperche <sylvain.laperche@gmail.com>
2016-12-12 19:24:59 +0100

    2016/06: add solution for Day 6 (Part 2)

--- 2016/Day 06 - Signals and Noise/src/main.rs
+++ 2016/Day 06 - Signals and Noise/src/main.rs
@@ -17,15 +17,49 @@ use std::fs::File;
 use std::io::Read;
 
 // }}}
-
+// {{{ Consensus computation
 
 /// Computes the original message from a list of corrupted messages.
 ///
 /// XXX: every messages should have the same length.
 ///
-/// Note that this problem is similar to the consensus sequence in bioinformatic
-/// (see http://rosalind.info/glossary/consensus-string/).
-fn compute_consensus(messages: &[&str]) -> String {
+/// For each letter, the chosen one is the most common letter among all the
+/// corrupted messages.
+fn compute_consensus_most_common(messages: &[&str]) -> String {
+    compute_consensus(messages, 0, |(cur_ch, cur_cnt), (ch, cnt)| {
+        if cnt > &cur_cnt {
+            (*ch, *cnt)
+        } else {
+            (cur_ch, cur_cnt)
+        }
+    })
+}
+
+/// Computes the original message from a list of corrupted messages.
+///
+/// XXX: every messages should have the same length.
+///
+/// For each letter, the chosen one is the least common letter among all the
+/// corrupted messages.
+fn compute_consensus_least_common(messages: &[&str]) -> String {
+    compute_consensus(messages, std::i32::MAX, |(cur_ch, cur_cnt), (ch, cnt)| {
+        if cnt <= &cur_cnt {
+            (*ch, *cnt)
+        } else {
+            (cur_ch, cur_cnt)
+        }
+    })
+}
+
+/// Computes the original message from a list of corrupted messages.
+///
+/// XXX: every messages should have the same length.
+///
+/// For each letter, the best candidate is chosen among all the corrupted
+/// messages according to the function `select`.
+fn compute_consensus<F>(messages: &[&str], init: i32, select: F) -> String
+    where F: Fn((char, i32), (&char, &i32)) -> (char, i32)
+{
     let msg_len = messages[0].len();
     let mut message = String::with_capacity(msg_len);
 
@@ -36,20 +70,16 @@ fn compute_consensus(messages: &[&str]) -> String {
             let ch = messages[msg_idx].as_bytes()[ch_idx] as char;
             *ch_freq.entry(ch).or_insert(0) += 1;
         }
-        // Find the most commont letter (iter::max_by is unstable ☹).
-        let (ch, _) = ch_freq.iter().fold((' ', 0), |(cur_ch, cur_cnt), (ch, cnt)| {
-            if cnt >= &cur_cnt {
-                (*ch, *cnt)
-            } else {
-                (cur_ch, cur_cnt)
-            }
-        });
+        // Find the letter that match the criterion.
+        let (ch, _) = ch_freq.iter().fold((' ', init), &select);
         message.push(ch);
 
         ch_freq.clear();
     }
     message
 }
+
+// }}}
 
 fn main() {
     let mut file  = File::open("input.txt").unwrap();
@@ -58,13 +88,14 @@ fn main() {
     file.read_to_string(&mut input).unwrap();
     let messages = input.lines().collect::<Vec<_>>();
 
-    println!("{:?}", compute_consensus(&messages));
+    println!("{:?}", compute_consensus_most_common(&messages));
+    println!("{:?}", compute_consensus_least_common(&messages));
 }
 
 // {{{ Tests
 
 #[test]
-fn examples_part1() {
+fn examples() {
     let messages = [
         "eedadn",
         "drvtee",
@@ -83,7 +114,8 @@ fn examples_part1() {
         "dvrsen",
         "enarar",
     ];
-    assert_eq!(compute_consensus(&messages), "easter");
+    assert_eq!(compute_consensus_most_common(&messages),  "easter");
+    assert_eq!(compute_consensus_least_common(&messages), "advent");
 }
 
 // }}}
