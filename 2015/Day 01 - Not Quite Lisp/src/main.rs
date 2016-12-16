7986a7700926621f9e39b27ff4a336db9d820127
Sylvain Laperche <sylvain.laperche@gmail.com>
2016-12-16 18:43:25 +0100

    2015/01: add solution for Day 1 (Part 2)

--- 2015/Day 01 - Not Quite Lisp/src/main.rs
+++ 2015/Day 01 - Not Quite Lisp/src/main.rs
@@ -18,14 +18,38 @@ use std::io::BufReader;
 
 // }}}
 
+/// Moves the next floor, according to the instruction.
+///
+/// The returned value is the next floor.
+fn move_to_next_floor(current_floor: i32, instruction: char) -> i32 {
+    match instruction {
+        '(' => current_floor + 1,
+        ')' => current_floor - 1,
+        _  => panic!("unexpected instruction: `{}`", instruction),
+    }
+}
+
+fn move_to_next_floor_mut(current_floor: &mut i32, instruction: char)
+    -> Option<i32>
+{
+    match instruction {
+        '(' => *current_floor += 1,
+        ')' => *current_floor -= 1,
+        _  => panic!("unexpected instruction: `{}`", instruction),
+    };
+    Some(*current_floor)
+}
+
+/// Computes the destination floor.
 fn compute_floor(directions : &str) -> i32 {
-    directions.chars().fold(0, |floor, c| {
-        match c {
-            '(' => floor + 1,
-            ')' => floor - 1,
-             _  => panic!("unexpected char: {}", c),
-        }
-    })
+    directions.chars().fold(0, move_to_next_floor)
+}
+
+/// Finds the first instruction that causes us to enter the basement (floor -1).
+fn find_instruction_to_basement(directions : &str) -> usize {
+    directions.chars().scan(0, move_to_next_floor_mut)
+                      .take_while(|floor | *floor != -1)
+                      .count() + 1
 }
 
 fn main() {
@@ -36,7 +60,9 @@ fn main() {
     BufReader::new(&file).read_line(&mut input).unwrap();
     assert!(!input.is_empty());
 
-    println!("Santa should go to the floor {}", compute_floor(&input.trim()));
+    println!("Santa should go to the floor {}.", compute_floor(&input.trim()));
+    println!("The first time Santa goes into the basement it at instruction {}.",
+             find_instruction_to_basement(&input.trim()));
 }
 
 // {{{ Tests
@@ -52,6 +78,12 @@ fn examples_part1() {
     assert_eq!(compute_floor("))("),     -1);
     assert_eq!(compute_floor(")))"),     -3);
     assert_eq!(compute_floor(")())())"), -3);
+}
+
+#[test]
+fn examples_part2() {
+    assert_eq!(find_instruction_to_basement(")"),     1);
+    assert_eq!(find_instruction_to_basement("()())"), 5);
 }
 
 // }}}
