da77666eab4b8d91a5ab56aa9382511f1574d521
Sylvain Laperche <sylvain.laperche@gmail.com>
2016-12-28 20:18:07 +0100

    2016/09: add solution for Day 9 (Part 2)
    Simple and naive recursive solution, better solutions certainly exists
    but this one does the job.

--- 2016/Day 09 - Explosives in Cyberspace/src/main.rs
+++ 2016/Day 09 - Explosives in Cyberspace/src/main.rs
@@ -27,19 +27,15 @@ mod compress {
     }
 
     /// Parses an usize value from a stream of bytes.
-    fn parse_usize<'a, I>(stream: &mut I) -> usize
-        where I: Iterator<Item=&'a u8>
-    {
+    fn parse_usize(stream: &mut Iterator<Item=&u8>) -> usize {
         stream.map(|&b| b as char).collect::<String>().parse().unwrap()
     }
 
     /// Extracts a marker from the stream.
-    fn extract_marker<'a, I>(stream: &mut I) -> Marker
-        where I: Iterator<Item=&'a u8>
-    {
+    fn extract_marker(stream: &mut Iterator<Item=&u8>) -> Marker {
         let mut bytes = stream.take_while(|&b| *b != b')');
         let length = parse_usize(&mut bytes.by_ref()
-                                           .take_while(|b| **b != b'x'));
+                                           .take_while(|&b| *b != b'x'));
         let repeat = parse_usize(&mut bytes);
         assert!(bytes.next().is_none());
 
@@ -47,26 +43,45 @@ mod compress {
     }
 
     /// Computes the size of the string after decompression.
-    pub fn compute_final_size(s : &str) -> u64 {
-        let mut bytes  = s.as_bytes().iter();
-        let mut size   = 0;
+    fn compute_final_size(mut bytes : &mut Iterator<Item=&u8>,
+                          do_recursive_expand: bool) -> u64
+    {
+        let mut size = 0;
 
         while let Some(&b) = bytes.next() {
             if b == b'(' {
-                let marker = extract_marker(&mut bytes);
-                // Skip `marker.length` bytes.
-                bytes.nth(marker.length - 1);
-                size += (marker.length*marker.repeat) as u64;
+                let marker   = extract_marker(&mut bytes);
+                let mut data = bytes.take(marker.length);
+                let len = if do_recursive_expand {
+                    compute_final_size(&mut data, true)
+                } else {
+                    // Consumes the iterator.
+                    let _ = data.last();
+                    marker.length as u64
+                };
+                size += len * (marker.repeat as u64);
             } else {
                 size += 1;
             }
         }
         size
     }
+
+    /// Computes the size of the string after decompression (algorithm v1).
+    pub fn compute_final_size_v1(s : &str) -> u64 {
+        compute_final_size(&mut s.as_bytes().iter(), false)
+    }
+
+    /// Computes the size of the string after decompression (algorithm v2).
+    ///
+    /// The v2 of the algorithm use recursive expansion of the markers.
+    pub fn compute_final_size_v2(s : &str) -> u64 {
+        compute_final_size(&mut s.as_bytes().iter(), true)
+    }
 }
 
 // }}}
-use compress::compute_final_size;
+use compress::{compute_final_size_v1,compute_final_size_v2};
 
 fn main() {
     let mut file  = File::open("input.txt").unwrap();
@@ -74,20 +89,33 @@ fn main() {
 
     file.read_to_string(&mut input).unwrap();
 
+    let input = input.trim();
     println!("The decompressed length of the file is {} bytes.",
-             compute_final_size(&input.trim()));
+             compute_final_size_v1(&input));
+    println!("The real decompressed length of the file is {} bytes.",
+             compute_final_size_v2(&input));
 }
 
 // {{{ Tests
 
 #[test]
 fn examples_part1() {
-    assert_eq!(compute_final_size("ADVENT"),             6);
-    assert_eq!(compute_final_size("A(1x5)BC"),           7);
-    assert_eq!(compute_final_size("(3x3)XYZ"),           9);
-    assert_eq!(compute_final_size("A(2x2)BCD(2x2)EFG"), 11);
-    assert_eq!(compute_final_size("(6x1)(1x3)A"),        6);
-    assert_eq!(compute_final_size("X(8x2)(3x3)ABCY"),   18);
+    assert_eq!(compute_final_size_v1("ADVENT"),             6);
+    assert_eq!(compute_final_size_v1("A(1x5)BC"),           7);
+    assert_eq!(compute_final_size_v1("(3x3)XYZ"),           9);
+    assert_eq!(compute_final_size_v1("A(2x2)BCD(2x2)EFG"), 11);
+    assert_eq!(compute_final_size_v1("(6x1)(1x3)A"),        6);
+    assert_eq!(compute_final_size_v1("X(8x2)(3x3)ABCY"),   18);
+}
+
+#[test]
+fn examples_part2() {
+    assert_eq!(compute_final_size_v2("(3x3)XYZ"),           9);
+    assert_eq!(compute_final_size_v2("X(8x2)(3x3)ABCY"),   20);
+    assert_eq!(compute_final_size_v2(
+            "(27x12)(20x12)(13x14)(7x10)(1x12)A"), 241920);
+    assert_eq!(compute_final_size_v2(
+            "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"), 445);
 }
 
 // }}}
