0c6b10b4a891115a275de111e6349015c09f2840
Sylvain Laperche <sylvain.laperche@gmail.com>
2017-07-03 21:58:11 +0200

    2015/05: add solution for Day 5 (Part 2)

--- 2015/Day 05 - Doesn't He Have Intern-Elves For This?/src/main.rs
+++ 2015/Day 05 - Doesn't He Have Intern-Elves For This?/src/main.rs
@@ -12,6 +12,8 @@
 // }}}
 // {{{ Crates
 
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 use std::fs::File;
 use std::io::Read;
 
@@ -43,6 +45,30 @@ fn is_nice_string(s: &str) -> bool {
     return false;
 }
 
+fn is_nice_string_v2(s: &str) -> bool {
+    let mut has_pair = false;
+    // Search a pair of any two letters that appears at least twice.
+    let mut pairs = HashMap::new();
+    for (i, pair) in s.as_bytes().windows(2).enumerate() {
+        match pairs.entry(pair) {
+            // First time we see this pair: insert the position.
+            Entry::Vacant(e)   => { e.insert(i); () }
+            // We know this pair, check for overlap!
+            Entry::Occupied(e) => if i - e.get() > 1 {
+                has_pair = true;
+                break;
+            }
+        };
+    }
+    // Search one letter which repeats with exactly one letter between them.
+    for triple in s.as_bytes().windows(3) {
+        if triple[0] == triple[2] {
+            return has_pair;
+        }
+    }
+    return false;
+}
+
 fn main() {
     let mut file  = File::open("input.txt").expect("cannot open input.txt");
     let mut input = String::new();
@@ -51,6 +77,8 @@ fn main() {
 
     println!("There are {} nice strings.",
              input.lines().filter(|&s| is_nice_string(s)).count());
+    println!("There are {} truly nice strings.",
+             input.lines().filter(|&s| is_nice_string_v2(s)).count());
 }
 
 // {{{ Tests
@@ -62,6 +90,16 @@ fn examples_part1() {
     assert_eq!(is_nice_string("jchzalrnumimnmhp"), false);
     assert_eq!(is_nice_string("haegwjzuvuyypxyu"), false);
     assert_eq!(is_nice_string("dvszwmarrgswjxmb"), false);
+}
+
+#[test]
+fn examples_part2() {
+    assert_eq!(is_nice_string_v2("qjhvhtzxzqqjkmpb"), true);
+    assert_eq!(is_nice_string_v2("xxyxx"),            true);
+    assert_eq!(is_nice_string_v2("aaaa"),             true);
+    assert_eq!(is_nice_string_v2("aaa"),              false);
+    assert_eq!(is_nice_string_v2("uurcxstgmygtbstg"), false);
+    assert_eq!(is_nice_string_v2("ieodomkazucvgmuy"), false);
 }
 
 // }}}
